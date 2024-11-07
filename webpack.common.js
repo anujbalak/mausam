d2ff9e60452d6f6a43aceb7ab52d11985ca23a36
Anuj <122350668+anujbalak@users.noreply.github.com>
2024-11-07 15:40:58 +0530

    Add Intial files

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            title: 'Mansoon',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/i,
+                use: ['html-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(png|jpeg|jpg|svg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
+
